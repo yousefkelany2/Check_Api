36388f5dc41b9d120a2dac3a010116f41e79a96e
yousef.abdmonem <128322134+yousefkelany2@users.noreply.github.com>
2025-05-25 21:53:18 +0300

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -116,9 +116,13 @@ class AuthController extends Controller
      * @return \Illuminate\Http\JsonResponse
      */
      public function refresh()
-    {
-        return $this->respondWithToken(auth($this->guard)->refresh());
-    } 
+   {
+    $newToken = auth($this->guard)->refresh();
+    auth($this->guard)->setToken($newToken);
+
+    return $this->respondWithToken($newToken);
+    }
+
 
     /**
      * Get the token array structure.
