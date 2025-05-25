0cde4198cf80a2b243bb6878b717ad09ecfdd363
yousef.abdmonem <128322134+yousefkelany2@users.noreply.github.com>
2025-05-25 21:54:25 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,14 +7,12 @@ use App\Http\Controllers\Api\AuthController;
 Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
 });
 
 
 Route::middleware('auth:api')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/profile', [AuthController::class, 'me']);
-    Route::put('/profile', [AuthController::class, 'updateProfile']);
-
+   
 
 
 });
